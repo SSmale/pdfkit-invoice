862a3a4015e54bcaef7d3fe209ac45ab4b8f67fc
Simon Smale <simon@smalemail.net>
2024-11-24 20:11:25 +0000

    feat: initial invoice

--- createInvoice.js
+++ createInvoice.js
@@ -1,98 +1,87 @@
 const fs = require("fs");
 const PDFDocument = require("pdfkit");
 
-function createInvoice(invoice, path) {
-  let doc = new PDFDocument({ size: "A4", margin: 50 });
+const lineSpacing = 12;
+const leftIndent = 30;
+const backgroundColour = '#F7F7F7';
+const highlightColour = '#E4E4E4'
+const headerColour = '#FFFFFF';
+const primaryColour = "#CC192F";
+const textColour = "#000000";
 
-  generateHeader(doc);
-  generateCustomerInformation(doc, invoice);
+function createInvoice(invoice, path) {
+  let doc = new PDFDocument({
+    size: "A4", margin: leftIndent
+  });
+
+  doc.rect(0, 0, doc.page.width, doc.page.height).fill(backgroundColour);
+
+  doc.registerFont('Bold', "fonts/Montserrat-Bold.ttf");
+  doc.registerFont('Regular', "fonts/Montserrat-Regular.ttf");
+
+  generateHeader(doc, invoice);
   generateInvoiceTable(doc, invoice);
-  generateFooter(doc);
+  generateFooter(doc, invoice);
 
   doc.end();
   doc.pipe(fs.createWriteStream(path));
 }
 
-function generateHeader(doc) {
+function generateHeader(doc, invoice) {
+  const initialY = 90
+  const rightCol = 200;
   doc
-    .image("logo.png", 50, 45, { width: 50 })
-    .fillColor("#444444")
-    .fontSize(20)
-    .text("ACME Inc.", 110, 57)
-    .fontSize(10)
-    .text("ACME Inc.", 200, 50, { align: "right" })
-    .text("123 Main Street", 200, 65, { align: "right" })
-    .text("New York, NY, 10025", 200, 80, { align: "right" })
+    .fillColor(primaryColour)
+    .font("Bold")
+    .fontSize(100)
+    .text("hello", leftIndent - 7, 13)
+    .fillColor(textColour)
+    .fontSize(26)
+    .font("Regular")
+    .text("This is your invoice.", leftIndent, 111)
+    .fontSize(12)
+    .text(`Invoice #${invoice.invoice_nr}`, leftIndent, initialY + (lineSpacing * 5))
+    .text(formatDate(new Date(invoice.date ?? Date.now())), leftIndent, initialY + (lineSpacing * 6))
+    .fontSize(16)
+    .text("Simon Smale", rightCol, initialY - 18, { align: "right" })
+    .font("Regular")
+    .fontSize(12)
+    .text("91 Walnut Avenue", 200, initialY, { align: "right" })
+    .text("Southampton", 200, initialY + (lineSpacing * 1), { align: "right" })
+    .text("Hampshire", 200, initialY + (lineSpacing * 2), { align: "right" })
+    .text("SO18 2QU", 200, initialY + (lineSpacing * 3), { align: "right" })
+    .text("simon@smalemail.net", 200, initialY + (lineSpacing * 5), { align: "right" })
+    .text("07872574140", 200, initialY + (lineSpacing * 6), { align: "right" })
     .moveDown();
-}
-
-function generateCustomerInformation(doc, invoice) {
-  doc
-    .fillColor("#444444")
-    .fontSize(20)
-    .text("Invoice", 50, 160);
-
-  generateHr(doc, 185);
-
-  const customerInformationTop = 200;
-
-  doc
-    .fontSize(10)
-    .text("Invoice Number:", 50, customerInformationTop)
-    .font("Helvetica-Bold")
-    .text(invoice.invoice_nr, 150, customerInformationTop)
-    .font("Helvetica")
-    .text("Invoice Date:", 50, customerInformationTop + 15)
-    .text(formatDate(new Date()), 150, customerInformationTop + 15)
-    .text("Balance Due:", 50, customerInformationTop + 30)
-    .text(
-      formatCurrency(invoice.subtotal - invoice.paid),
-      150,
-      customerInformationTop + 30
-    )
-
-    .font("Helvetica-Bold")
-    .text(invoice.shipping.name, 300, customerInformationTop)
-    .font("Helvetica")
-    .text(invoice.shipping.address, 300, customerInformationTop + 15)
-    .text(
-      invoice.shipping.city +
-        ", " +
-        invoice.shipping.state +
-        ", " +
-        invoice.shipping.country,
-      300,
-      customerInformationTop + 30
-    )
-    .moveDown();
-
-  generateHr(doc, 252);
 }
 
 function generateInvoiceTable(doc, invoice) {
   let i;
-  const invoiceTableTop = 330;
+  const invoiceTableTop = 190;
+  const startWidth = 595 - 250;
+  doc.rect(leftIndent, invoiceTableTop, startWidth - 2, 16).fill(primaryColour);
+  doc.rect(doc.page.width - leftIndent - 190, invoiceTableTop, 50 - 2, 16).fill(primaryColour);
+  doc.rect(doc.page.width - leftIndent - 140, invoiceTableTop, 70 + 2, 16).fill(primaryColour);
+  doc.rect(doc.page.width - leftIndent - 70 + 4, invoiceTableTop, 70 - 4, 16).fill(primaryColour);
 
-  doc.font("Helvetica-Bold");
   generateTableRow(
     doc,
     invoiceTableTop,
-    "Item",
     "Description",
-    "Unit Cost",
-    "Quantity",
-    "Line Total"
+    "Unit Price",
+    "Qty",
+    "Amount",
+    "Bold",
+    12,
+    headerColour
   );
-  generateHr(doc, invoiceTableTop + 20);
-  doc.font("Helvetica");
 
   for (i = 0; i < invoice.items.length; i++) {
     const item = invoice.items[i];
-    const position = invoiceTableTop + (i + 1) * 30;
+    const position = invoiceTableTop - 6 + (i + 1) * 30;
     generateTableRow(
       doc,
       position,
-      item.item,
       item.description,
       formatCurrency(item.amount / item.quantity),
       item.quantity,
@@ -101,91 +90,111 @@ function generateInvoiceTable(doc, invoice) {
 
     generateHr(doc, position + 20);
   }
-
-  const subtotalPosition = invoiceTableTop + (i + 1) * 30;
-  generateTableRow(
-    doc,
-    subtotalPosition,
-    "",
-    "",
-    "Subtotal",
-    "",
-    formatCurrency(invoice.subtotal)
-  );
-
-  const paidToDatePosition = subtotalPosition + 20;
-  generateTableRow(
-    doc,
-    paidToDatePosition,
-    "",
-    "",
-    "Paid To Date",
-    "",
-    formatCurrency(invoice.paid)
-  );
-
-  const duePosition = paidToDatePosition + 25;
-  doc.font("Helvetica-Bold");
-  generateTableRow(
-    doc,
-    duePosition,
-    "",
-    "",
-    "Balance Due",
-    "",
-    formatCurrency(invoice.subtotal - invoice.paid)
-  );
-  doc.font("Helvetica");
 }
 
-function generateFooter(doc) {
+function generateFooter(doc, invoice) {
+  const totalBarStartY = doc.page.height - 200 - 168;
+  if (doc.y >= totalBarStartY) {
+    doc.addPage()
+  }
+
+  doc.rect(0, totalBarStartY, doc.page.width, 168).fill(highlightColour);
+  doc
+    .fillColor(textColour)
+    .fontSize(10)
+    .text("FOR", leftIndent, totalBarStartY + 32)
+    .font("Bold")
+    .fontSize(14)
+    .moveDown(0).text(invoice.customer.name)
+    .font("Regular")
+    .fontSize(12)
+    .moveDown(0).text(invoice.customer.addressLine1)
+    .moveDown(0).text(invoice.customer.addressLine2)
+    .moveDown(0).text(invoice.customer.town)
+    .moveDown(0).text(invoice.customer.postCode)
+    .moveDown(0).text(invoice.customer.email)
+  doc
+    .strokeColor(primaryColour)
+    .lineWidth(2)
+    .moveTo(leftIndent + (doc.page.width / 2), doc.page.height - 200 - 83 - 26)
+    .lineTo(doc.page.width - leftIndent, doc.page.height - 200 - 83 - 26)
+    .stroke();
+  doc
+    .fillColor(textColour)
+    .font("Bold")
+    .fontSize(14)
+    .text("Total", leftIndent + (doc.page.width / 2) + 10, doc.page.height - 200 - 83 - 8)
+  const total = invoice.items.reduce((t, i) => t += (i.amount * i.quantity), 0);  
+  doc
+    .fillColor(textColour)
+    .font("Regular")
+    .fontSize(14)
+    .text(formatCurrency(total), leftIndent + (doc.page.width / 2) + 130, doc.page.height - 200 - 83 - 8, { width: 100, align: "right" })
+  doc
+    .strokeColor(primaryColour)
+    .lineWidth(2)
+    .moveTo(leftIndent + (doc.page.width / 2), doc.page.height - 200 - 52)
+    .lineTo(doc.page.width - leftIndent, doc.page.height - 200 - 52)
+    .stroke();
+  doc
+    .fillColor(primaryColour)
+    .font("Regular")
+    .fontSize(40)
+    .text("thank you!", leftIndent - 7, doc.page.height - 170)
+  doc.rect(0, doc.page.height - 90, doc.page.width, 90).fill(highlightColour);
   doc
     .fontSize(10)
+    .fillColor(textColour)
+    .font("Bold")
     .text(
-      "Payment is due within 15 days. Thank you for your business.",
-      50,
-      780,
-      { align: "center", width: 500 }
-    );
+      "Bank Account",
+      0,
+      doc.page.height - 70,
+      { align: "center", width: doc.page.width }
+    ).font("Regular")
+    .moveDown(0).text(
+      "Account # : 53237370", { align: "center", width: doc.page.width }).moveDown(0).text(
+        "Sort Code : 04-00-04", { align: "center", width: doc.page.width })
 }
 
 function generateTableRow(
   doc,
   y,
-  item,
   description,
   unitCost,
   quantity,
-  lineTotal
+  lineTotal,
+  font = "Regular",
+  fontSize = 10,
+  colour = textColour
 ) {
   doc
-    .fontSize(10)
-    .text(item, 50, y)
-    .text(description, 150, y)
-    .text(unitCost, 280, y, { width: 90, align: "right" })
-    .text(quantity, 370, y, { width: 90, align: "right" })
-    .text(lineTotal, 0, y, { align: "right" });
+    .font(font)
+    .fontSize(fontSize)
+    .fillColor(colour)
+    .text(description, leftIndent + 2, y)
+    .text(quantity, doc.page.width - leftIndent - 190 - 2, y, { width: 50, align: "center" })
+    .text(unitCost, doc.page.width - leftIndent - 140 - 2, y, { width: 70, align: "right" })
+    .text(lineTotal, doc.page.width - leftIndent - 70 - 2, y, { width: 70, align: "right" })
+    .moveDown();
 }
 
 function generateHr(doc, y) {
   doc
-    .strokeColor("#aaaaaa")
-    .lineWidth(1)
-    .moveTo(50, y)
-    .lineTo(550, y)
-    .stroke();
+    .strokeColor(primaryColour)
+    .lineWidth(2)
+    .moveTo(leftIndent, y)
+    .lineTo(doc.page.width - leftIndent, y)
+    .stroke()
+    .moveDown();
 }
 
-function formatCurrency(cents) {
-  return "$" + (cents / 100).toFixed(2);
+function formatCurrency(pence) {
+  return "£" + (pence / 100).toFixed(2);
 }
 
 function formatDate(date) {
-  const day = date.getDate();
-  const month = date.getMonth() + 1;
-  const year = date.getFullYear();
-
-  return year + "/" + month + "/" + day;
+  return date.toLocaleString("en-GB", { day: 'numeric', month: 'long', year: 'numeric' });
 }
 
 module.exports = {
--- index.js
+++ index.js
@@ -1,31 +1,118 @@
 const { createInvoice } = require("./createInvoice.js");
 
 const invoice = {
-  shipping: {
-    name: "John Doe",
-    address: "1234 Main Street",
-    city: "San Francisco",
-    state: "CA",
-    country: "US",
-    postal_code: 94111
+  date: '2024-11-30',
+  customer: {
+    name: "The Change Mavericks",
+    addressLine1:"1 Cedar Office Park",
+    addressLine2:"Cobham Road",
+    town:"Ferndown",
+    postCode: "BH21 7SB",
+    email:"accounts@thechangemavericks.com"
   },
   items: [
     {
-      item: "TC 100",
       description: "Toner Cartridge",
       quantity: 2,
       amount: 6000
     },
     {
-      item: "USB_EXT",
+      description: "USB Cable Extender",
+      quantity: 1,
+      amount: 2000
+    },
+    {
+      description: "USB Cable Extender",
+      quantity: 1,
+      amount: 2000
+    },
+    {
+      description: "USB Cable Extender",
+      quantity: 1,
+      amount: 2000
+    },
+    {
+      description: "USB Cable Extender",
+      quantity: 1,
+      amount: 2000
+    },
+    {
+      description: "USB Cable Extender",
+      quantity: 1,
+      amount: 2000
+    },
+    {
+      description: "USB Cable Extender",
+      quantity: 1,
+      amount: 2000
+    },
+    {
+      description: "USB Cable Extender",
+      quantity: 1,
+      amount: 2000
+    },
+    {
+      description: "USB Cable Extender",
+      quantity: 1,
+      amount: 2000
+    },
+    {
+      description: "USB Cable Extender",
+      quantity: 1,
+      amount: 2000
+    },
+    {
+      description: "USB Cable Extender",
+      quantity: 1,
+      amount: 2000
+    },
+    {
+      description: "USB Cable Extender",
+      quantity: 1,
+      amount: 2000
+    },
+    {
+      description: "USB Cable Extender",
+      quantity: 1,
+      amount: 2000
+    },
+    {
+      description: "USB Cable Extender",
+      quantity: 1,
+      amount: 2000
+    },
+    {
+      description: "USB Cable Extender",
+      quantity: 1,
+      amount: 2000
+    },
+    {
+      description: "USB Cable Extender",
+      quantity: 1,
+      amount: 2000
+    },
+    {
+      description: "USB Cable Extender",
+      quantity: 1,
+      amount: 2000
+    },
+    {
+      description: "USB Cable Extender",
+      quantity: 1,
+      amount: 2000
+    },
+    {
+      description: "USB Cable Extender",
+      quantity: 1,
+      amount: 2000
+    },
+    {
       description: "USB Cable Extender",
       quantity: 1,
       amount: 2000
     }
   ],
-  subtotal: 8000,
-  paid: 0,
-  invoice_nr: 1234
+  invoice_nr: 12534
 };
 
 createInvoice(invoice, "invoice.pdf");
